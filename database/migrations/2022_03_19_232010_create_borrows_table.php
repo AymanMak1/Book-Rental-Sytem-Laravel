76e426c8f46e4a8520ff57167e000e98916e1ebf
Webstain <ayman.makhoukhi@gmail.com>
2022-03-20 00:59:26 +0100

    working on borrows table migration

--- database/migrations/2022_03_19_232010_create_borrows_table.php
+++ database/migrations/2022_03_19_232010_create_borrows_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('borrows', function (Blueprint $table) {
             $table->id();
+           // $table->foreign('reader_id')->references('id')->on('users')->onDelete('cascade');
+          //  $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+            $table->enum('status',['PENDING', 'ACCEPTED', 'REJECTED', 'RETURNED']);
+            $table->timestamp('request_processed_at')->nullable();
+         //   $table->foreign('request_managed_by')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamp('deadline')->nullable();
+            $table->timestamp('returned_at')->nullable();
+          //  $table->foreign('return_managed_by')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
