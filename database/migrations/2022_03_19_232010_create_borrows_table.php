15ee25d0c7f8e8e2aae51995d68707b4fdf2d978
Webstain <ayman.makhoukhi@gmail.com>
2022-03-20 01:08:08 +0100

    Foreign keys added to the borrows table

--- database/migrations/2022_03_19_232010_create_borrows_table.php
+++ database/migrations/2022_03_19_232010_create_borrows_table.php
@@ -15,14 +15,18 @@ return new class extends Migration
     {
         Schema::create('borrows', function (Blueprint $table) {
             $table->id();
-           // $table->foreign('reader_id')->references('id')->on('users')->onDelete('cascade');
-          //  $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+            $table->unsignedBigInteger('reader_id');
+            $table->foreign('reader_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('book_id');
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
             $table->enum('status',['PENDING', 'ACCEPTED', 'REJECTED', 'RETURNED']);
             $table->timestamp('request_processed_at')->nullable();
-         //   $table->foreign('request_managed_by')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('request_managed_by')->nullable();
+            $table->foreign('request_managed_by')->references('id')->on('users')->onDelete('cascade');
             $table->timestamp('deadline')->nullable();
             $table->timestamp('returned_at')->nullable();
-          //  $table->foreign('return_managed_by')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('return_managed_by')->nullable();
+            $table->foreign('return_managed_by')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
